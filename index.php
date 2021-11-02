ed035fcdb1a63855f182a881177a4dbe6ec12114
Karim SEBBAH <Karim.Sebbah@etu.univ-paris1.fr>
2021-11-02 14:30:07 +0100

    ajout des template inscription et connexion

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php session_start();
+
+//hypothèse 2 paramètres d'entrée, controle et action, avec l'url de index.php
+// exmple : index.php?controle=entreprise&action=entrepriseNonAband
+
+if (isset($_GET['controle']) & isset($_GET['action'])) {
+ 	$controle = $_GET['controle'];
+	$action= $_GET['action'];
+	}
+else { //absence de paramètres : prévoir des valeurs par défaut
+	$controle = "inscription";
+	$action= "connexion";
+	}
+	
+//inclure le fichier php de contrôle 
+//et lancer la fonction-action issue de ce fichier.	
+
+	require ('./controle/' . $controle . '.php');   
+	$action (); 
+?>
