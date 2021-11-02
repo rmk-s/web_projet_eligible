f790597625c939a803c76a3be9477fb59be0622d
Karim SEBBAH <Karim.Sebbah@etu.univ-paris1.fr>
2021-11-02 15:46:49 +0100

    update formulaire

--- controleur/inscription.php
+++ controleur/inscription.php
@@ -12,24 +12,23 @@
 			//require ("./vue/inscription.tpl") ;
         }
 		else {
-			if ($nom == ''|$prenom == ''|$num == ''|$email == '') { //dans le cas où au moins un champ serait vide
+			if ($nom == ''|$prenom == ''|$mail == ''|$mdp == '') { //dans le cas où au moins un champ serait vide
 					$message ="Remplir l'ensemble des champs présents.";
-					//require ("./vue/inscription.tpl");}
-			else {
-                if  (!verif_ident($mail,$mdp)) { //Verifie que ce compte n'existe pas deja dans la BD
-										
-                    $nom=ucfirst(strtolower($nom)); // on met la 1ère lettre du nom en majuscule et le reste en minuscule
-                    $prenom=ucfirst(strtolower($prenom)); // on met la 1ère lettre du prénom en majuscule et le reste en minuscule
-                    ajouter_utilisateur($nom, $prenom, $mail, $mdp); //On ajoute l'utilsateur dans notre base
-                    //require("./vue/bienvenue.tpl");
-                }			
-                else {
-                    $message =(" Vous êtes déjà inscrit(e). Utiliser la page connexion pour vous connecter");
-                    //require ("./vue/inscription.tpl");}
+					require ("./vue/inscription.tpl");
+            } 
+            else{
+                if  (!verif_user($mail,$mdp)) { //Verifie que ce compte n'existe pas deja dans la BD
+                                            
+                        $nom=ucfirst(strtolower($nom)); // on met la 1ère lettre du nom en majuscule et le reste en minuscule
+                        $prenom=ucfirst(strtolower($prenom)); // on met la 1ère lettre du prénom en majuscule et le reste en minuscule
+                        new_user($nom, $prenom, $mail, $mdp); //On ajoute l'utilsateur dans notre base
+                        //require("./vue/bienvenue.tpl");
+                    }			
+                    else {
+                        $message =(" Vous êtes déjà inscrit(e). Utiliser la page connexion pour vous connecter");
+                        //require ("./vue/inscription.tpl");}
+                    }
+                }
             }
-    }
-
-
-
-
+        }
 ?>
