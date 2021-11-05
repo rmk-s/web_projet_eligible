e3ef7c6b15ec088ff4acd0c58bfc084d6d607530
Karim SEBBAH <Karim.Sebbah@etu.univ-paris1.fr>
2021-11-05 01:45:53 +0100

    test

--- control/inscription.php
+++ control/inscription.php
@@ -2,26 +2,30 @@
 
     function inscrire(){
         
-        $num = isset($POST['num']) ? ($_POST['num']):'';
         $nom = isset($POST['nom']) ? ($_POST['nom']):'';
         $prenom = isset($POST['prenom']) ? ($_POST['prenom']):'';
         $mail = isset($POST['mail']) ? ($_POST['mail']):'';
         $mdp = isset($POST['mdp']) ? ($_POST['mdp']):'';
-        $message=''; 
+        $message=' '; 
+
+        var_dump($nom);
 
         require("./model/inscriptionBD.php");
         
-		if ($nom == ''|$prenom == ''|$mail == ''|$mdp == ''|$num == '') { //dans le cas où au moins un champ serait vide
-			$message ="Remplir l'ensemble des champs présents.";
+		if ($nom == ''|$prenom == ''|$mail == ''|$mdp == '') { //dans le cas où au moins un champ serait vide
 			require ("./vue/pageInscription.tpl");
+            require ("./vue/pageInscription.tpl");
             } 
             else{
                 if  (!verif_user($mail,$mdp)) { //Verifie que ce compte n'existe pas deja dans la BD
                                             
                         $nom=ucfirst(strtolower($nom)); // on met la 1ère lettre du nom en majuscule et le reste en minuscule
                         $prenom=ucfirst(strtolower($prenom)); // on met la 1ère lettre du prénom en majuscule et le reste en minuscule
-                        new_user($num, $nom, $prenom, $mail, $mdp); //On ajoute l'utilsateur dans notre base
-                        require("./vue/pageUser.tpl");
+                        
+                    $profil = array();    
+                    new_user($nom, $prenom, $mail, $mdp, $profil); //On ajoute l'utilsateur dans notre base
+                    $message="La connexion à fonctionner !";    
+                    require("./vue/pageUser.tpl");
                     }			
                     else {
                         $message =(" Vous êtes déjà inscrit(e). Utiliser la page connexion pour vous connecter");
